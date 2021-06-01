2fc5d6ae804a5ece221b6ee314b89b778d011307
raghav94603 <34892373+raghav94603@users.noreply.github.com>
2021-06-01 17:53:47 +0530

    Update new.js

--- new.js
+++ new.js
@@ -4,7 +4,6 @@ class food
 constructor()
 {
     var food_name;
-    var f_quantity;
 }
 getFoodname()
 {
@@ -14,17 +13,10 @@ setfoodName(food_name)
 {
     this.food_name=food_name;
 }
-getquantity()
-{
-    return this.f_quantity;
+
 }
-setQuantity(f_quantity)
-{
-this.f_quantity=this.f_quantity;
-}
-}
+
 
 var inputt=new food();
 inputt.setfoodName("Pizza");
-inputt.setQuantity(8);
-console.log(inputt.getFoodname()+" "+inputt.getquantity());
+console.log(inputt.getFoodname());
